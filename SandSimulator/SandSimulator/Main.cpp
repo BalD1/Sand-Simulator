9bcf6fbb813834dadd6420db57f6a4ffa2542f0e
Florian <florianhusson@hotmail.fr>
2024-01-30 10:44:10 +0100

    color gradiant

--- SandSimulator/SandSimulator/Main.cpp
+++ SandSimulator/SandSimulator/Main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <random>
+#include <map>
 
 const int winSizeX = 800;
 const int winSizeY = 800;
@@ -16,8 +17,8 @@ const float cellBaseFallTime = .15f;
 
 const float gravity = 9.81f;
 
-const sf::Color fullCellColor_0 = sf::Color(0,0,0);
-const sf::Color fullCellColor_1 = sf::Color(255,255,255);
+const sf::Color fullCellColor_0 = sf::Color(215, 198, 143);
+const sf::Color fullCellColor_1 = sf::Color(228,214,172);
 
 static sf::RectangleShape emptyCell;
 static sf::RectangleShape fullCell;
@@ -29,43 +30,50 @@ const int fullCellID = 1;
 const int wallCellID = 2;
 const int holeCellID = 3;
 
-void DrawGrid(int grid[gridSizeX][gridSizeY], sf::RenderWindow& window)
+const std::map<int, sf::Color> cellsColor
+{
+    {0, sf::Color::Transparent},
+    {2, sf::Color::Red},
+    {3, sf::Color::Transparent},
+};
+
+struct CellData
+{
+    int ID;
+    sf::Color color;
+
+    float fallTimer;
+    int fallMultiplier;
+};
+
+void DrawGrid(CellData grid[gridSizeX][gridSizeY], sf::RenderWindow& window)
 {
     window.clear(sf::Color(18, 33, 43));
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distr_r(fullCellColor_0.r, fullCellColor_1.r);
-    std::uniform_int_distribution<> distr_g(fullCellColor_0.g, fullCellColor_1.g);
-    std::uniform_int_distribution<> distr_b(fullCellColor_0.b, fullCellColor_1.b);
-
-    sf::Color color;
 
     for (size_t x = 0; x < gridSizeX; x++)
     {
         for (size_t y = 0; y < gridSizeY; y++)
         {
-            switch (grid[x][y])
+            switch (grid[x][y].ID)
             {
                 case emptyCellID:
                     emptyCell.setPosition(cellSizeX * x, cellSizeY * y);
+                    emptyCell.setFillColor(grid[x][y].color);
                     window.draw(emptyCell);
                     break;
                 case fullCellID:
-                    color = sf::Color(distr_r(gen),
-                                      distr_g(gen),
-                                      distr_b(gen));
-                    fullCell.setFillColor(color);
-
+                    fullCell.setFillColor(grid[x][y].color);
                     fullCell.setPosition(cellSizeX * x, cellSizeY * y);
                     window.draw(fullCell);
                     break;
                 case wallCellID:
                     wallCell.setPosition(cellSizeX * x, cellSizeY * y);
+                    wallCell.setFillColor(grid[x][y].color);
                     window.draw(wallCell);
                     break;
                 case holeCellID:
                     holeCell.setPosition(cellSizeX * x, cellSizeY * y);
+                    holeCell.setFillColor(grid[x][y].color);
                     window.draw(holeCell);
                     break;
             }
@@ -74,27 +82,26 @@ void DrawGrid(int grid[gridSizeX][gridSizeY], sf::RenderWindow& window)
     window.display();
 }
 
-void ProcessGrid(int(&grid)[gridSizeX][gridSizeY], std::tuple<float, int>(&fallTimeGrid)[gridSizeX][gridSizeY], float dt)
+void ProcessGrid(CellData(&grid)[gridSizeX][gridSizeY], float dt)
 {
     bool leftToRight = rand() % 100 < 50;
     for (int x = leftToRight ? 0 : gridSizeX - 1; leftToRight ? x < gridSizeX : x >= 0; leftToRight ? x++ : x--)
     {
         for (int y = gridSizeY - 2; y >= 0; y--)
         {
-            // if is full
-            if (grid[x][y] == 1)
+            if (grid[x][y].ID == fullCellID)
             {
-                if (grid[x][y + 1] == wallCellID) continue;
-                bool isBEmpty = (grid[x][y + 1] == emptyCellID) || (grid[x][y + 1] == holeCellID);
+                if (grid[x][y + 1].ID == wallCellID) continue;
+                bool isBEmpty = (grid[x][y + 1].ID == emptyCellID) || (grid[x][y + 1].ID == holeCellID);
                 bool isBRightEmpty = x < gridSizeX && 
-                                    ((grid[x + 1][y + 1] == emptyCellID) || (grid[x + 1][y + 1] == holeCellID)) &&
-                                    ((grid[x + 1][y] == emptyCellID) || (grid[x + 1][y] == holeCellID));
+                                    ((grid[x + 1][y + 1].ID == emptyCellID) || (grid[x + 1][y + 1].ID == holeCellID)) &&
+                                    ((grid[x + 1][y].ID == emptyCellID) || (grid[x + 1][y].ID == holeCellID));
                 bool isBLeftEmpty = x > 0 && 
-                                    ((grid[x - 1][y + 1] == emptyCellID) || (grid[x - 1][y + 1] == holeCellID)) &&
-                                    ((grid[x - 1][y] == emptyCellID) || (grid[x - 1][y] == holeCellID));
+                                    ((grid[x - 1][y + 1].ID == emptyCellID) || (grid[x - 1][y + 1].ID == holeCellID)) &&
+                                    ((grid[x - 1][y].ID == emptyCellID) || (grid[x - 1][y].ID == holeCellID));
 
-                float& timer = std::get<0>(fallTimeGrid[x][y]);
-                int& incrModifier = std::get<1>(fallTimeGrid[x][y]);
+                float& timer = grid[x][y].fallTimer;
+                int& incrModifier = grid[x][y].fallMultiplier;
 
                 // to refactor
                 // if none of below are empty
@@ -139,38 +146,57 @@ void ProcessGrid(int(&grid)[gridSizeX][gridSizeY], std::tuple<float, int>(&fallT
                     else if (isBLeftEmpty)
                         nextCell = sf::Vector2i(x - 1, y + cellsFallIdx);
                 }
-                grid[x][y] = emptyCellID;
+
+                CellData lastCellData = grid[x][y];
+                grid[x][y].ID = emptyCellID;
+                grid[x][y].color = cellsColor.at(emptyCellID);
 
                 for (size_t i = 1; i <= cellsFallIdx; i++)
                 {
-                    if (grid[nextCell.x][y + i] == fullCellID)
+                    if (grid[nextCell.x][y + i].ID == fullCellID)
                     {
                         nextCell.y -= (cellsFallIdx - (i -1));
                         break;
                     }
                 }
-                if (grid[nextCell.x][nextCell.y] == holeCellID) continue;
-                
-                grid[nextCell.x][nextCell.y] = fullCellID;
+                if (grid[nextCell.x][nextCell.y].ID == holeCellID) continue;
 
-                int& nextIncrModifier = std::get<1>(fallTimeGrid[nextCell.x][nextCell.y]);
+                grid[nextCell.x][nextCell.y].ID = fullCellID;
+                grid[nextCell.x][nextCell.y].color = lastCellData.color;
+
+                int& nextIncrModifier = grid[nextCell.x][nextCell.y].fallMultiplier;
                 nextIncrModifier = incrModifier + 1;
-                float& nextTimer = std::get<0>(fallTimeGrid[nextCell.x][nextCell.y]);
+                float& nextTimer = grid[nextCell.x][nextCell.y].fallTimer;
                 nextTimer = cellBaseFallTime + timer;
             }
         }
     }
 }
 
-void SpawnCell(int(&grid)[gridSizeX][gridSizeY], std::tuple<float, int>(&fallTimeGrid)[gridSizeX][gridSizeY], sf::Vector2i pos, int cellType)
+void SpawnCell(CellData(&grid)[gridSizeX][gridSizeY], sf::Vector2i pos, int cellType)
 {
     if (pos.x < 0 || pos.x >= gridSizeX) return;
     if (pos.y < 0 || pos.y >= gridSizeY) return;
-    if (grid[pos.x][pos.y] == cellType) return;
-    grid[pos.x][pos.y] = cellType;
+    if (grid[pos.x][pos.y].ID == cellType) return;
+    grid[pos.x][pos.y].ID = cellType;
 
-    if (cellType == 1)
-        fallTimeGrid[pos.x][pos.y] = { cellBaseFallTime, 1 };
+    if (cellType == fullCellID)
+    {
+        grid[pos.x][pos.y].fallTimer = cellBaseFallTime;
+        grid[pos.x][pos.y].fallMultiplier = 1;
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distr_r(fullCellColor_0.r, fullCellColor_1.r);
+        std::uniform_int_distribution<> distr_g(fullCellColor_0.g, fullCellColor_1.g);
+        std::uniform_int_distribution<> distr_b(fullCellColor_0.b, fullCellColor_1.b);
+
+        grid[pos.x][pos.y].color = sf::Color(distr_r(gen),
+            distr_g(gen),
+            distr_b(gen));
+    }
+    else if (cellsColor.count(cellType))
+        grid[pos.x][pos.y].color = cellsColor.at(cellType);
 }
 
 sf::Vector2i GetGridMousePos(sf::RenderWindow& window)
@@ -184,34 +210,28 @@ int main()
     window.setFramerateLimit(60);
 
     emptyCell.setSize(sf::Vector2f(cellSizeX, cellSizeY));
-    emptyCell.setFillColor(sf::Color::Transparent);
     emptyCell.setOutlineColor(sf::Color::Black);
     emptyCell.setOutlineThickness(1);
 
     fullCell.setSize(sf::Vector2f(cellSizeX, cellSizeY));
-    fullCell.setFillColor(sf::Color::White);
     fullCell.setOutlineColor(sf::Color::Black);
     fullCell.setOutlineThickness(1);
 
     wallCell.setSize(sf::Vector2f(cellSizeX, cellSizeY));
-    wallCell.setFillColor(sf::Color::Red);
     wallCell.setOutlineColor(sf::Color::Black);
     wallCell.setOutlineThickness(1);
 
     holeCell.setSize(sf::Vector2f(cellSizeX, cellSizeY));
-    holeCell.setFillColor(sf::Color::Transparent);
     holeCell.setOutlineColor(sf::Color::Red);
     holeCell.setOutlineThickness(2);
 
-    int grid[gridSizeX][gridSizeY];
-    std::tuple<float, int> fallTimeGrid[gridSizeX][gridSizeY];
+    CellData grid[gridSizeX][gridSizeY];
 
     for (size_t x = 0; x < gridSizeX; x++)
     {
         for (size_t y = 0; y < gridSizeY; y++)
         {
-            grid[x][y] = 0;
-            fallTimeGrid[x][y] = { 0,0 };
+            grid[x][y] = {0, sf::Color::Transparent, 0, 0};
         }
     }
 
@@ -257,7 +277,7 @@ int main()
                     if (event.mouseButton.button == sf::Mouse::Right)
                     {
                         sf::Vector2i mousePos = GetGridMousePos(window);
-                        std::cout << "x:" + std::to_string(mousePos.x) + " y:" + std::to_string(mousePos.y) + " : " + std::to_string(grid[mousePos.x][mousePos.y]) << std::endl;
+                        std::cout << "x:" + std::to_string(mousePos.x) + " y:" + std::to_string(mousePos.y) + " : " + std::to_string(grid[mousePos.x][mousePos.y].ID) << std::endl;
                     }
                     break;
                 case sf::Event::MouseButtonReleased:
@@ -272,9 +292,9 @@ int main()
         {
             cellSpawnTimer = cellSpawnCD;
             sf::Vector2i mouseGridPos = GetGridMousePos(window);
-            SpawnCell(grid, fallTimeGrid, mouseGridPos, cellType);
+            SpawnCell(grid, mouseGridPos, cellType);
         }
-        ProcessGrid(grid, fallTimeGrid, dt.asSeconds());
+        ProcessGrid(grid, dt.asSeconds());
         DrawGrid(grid, window);
         dt = clock.restart();
     }
